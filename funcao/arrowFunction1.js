9246cf7384abc9dc0037b4fca383fe0b5005ab2b
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-02-22 21:24:54 -0300

    more about arrow functions

---
+++ funcao/arrowFunction1.js
@@ -0,0 +1,13 @@
+// padrão
+let dobro = function (num) {
+    return num * 2
+}
+
+// arrow normal
+dobro = (num) => {
+    return num * 2
+}
+
+// arrow retorno implícito (apenas 1 sentença)
+dobro = num => num * 2
+
---
+++ funcao/arrowFunction3.js
@@ -0,0 +1,18 @@
+let comparaComThis = function (param) {
+    console.log(this === param)
+}
+
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj) // não funciona, pois a função arrow é "mais forte" que o bind
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)
