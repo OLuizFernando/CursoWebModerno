f5c69ea9b08abfea86b843dc5e01e81bbb1f9d8c
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2024-07-12 15:39:31 -0300

    diferenças entre objetos e funções

--- funcao/funcaoConstrutora.js
+++ funcao/funcaoConstrutora.js
@@ -20,3 +20,10 @@ function Carro(velocidadeMaxima = 200, delta = 5) {
 const uno = new Carro
 uno.acelerar()
 console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
+
+console.log(typeof Carro) // Carro é uma funcao
+console.log(typeof ferrari) // ferrari é um objeto
